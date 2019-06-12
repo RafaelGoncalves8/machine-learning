46b891ab8467afcd90b816ecaecc4f23d2659dc8
Rafael Goncalves <rafaelgoncalves@riseup.net>
2019-06-12 19:39:02 -0300

    Add dropout

--- 3-mlp-and-svm/deliver/EFC3 - MLP and SVM.py
+++ 3-mlp-and-svm/deliver/EFC3 - MLP and SVM.py
@@ -3,7 +3,7 @@
 
 # Rafael Gonçalves (186062)
 
-# In[66]:
+# In[1]:
 
 
 import os
@@ -12,17 +12,25 @@ import random
 import numpy as np
 import scipy.io
 import matplotlib.pyplot as plt
+import gc
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.model_selection import GridSearchCV
+
+from mlxtend.plotting import plot_decision_regions
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 # ## Download dataset
 
-# In[67]:
+# In[2]:
 
 
 train_url = 'http://www.dca.fee.unicamp.br/~lboccato/dados_treinamento.mat'
@@ -39,7 +47,7 @@ urllib.request.urlretrieve(validation_url, validation_path)
 urllib.request.urlretrieve(test_url, test_path)
 
 
-# In[68]:
+# In[3]:
 
 
 train_set = scipy.io.loadmat(train_path)
@@ -47,7 +55,7 @@ val_set = scipy.io.loadmat(validation_path)
 test_set = scipy.io.loadmat(test_path)
 
 
-# In[69]:
+# In[4]:
 
 
 X_train = train_set["X"]
@@ -58,7 +66,7 @@ X_test = test_set["Xt"]
 y_test = test_set["yt"]
 
 
-# In[70]:
+# In[5]:
 
 
 y_train[y_train == -1] = 0
@@ -66,38 +74,55 @@ y_val[y_val == -1] = 0
 y_test[y_test == -1] = 0
 
 
-# In[71]:
+# In[6]:
 
 
 plt.plot(X_train[np.in1d(y_train, 1), 0], X_train[np.in1d(y_train, 1), 1], 'rX')
 plt.plot(X_train[np.in1d(y_train, 0), 0], X_train[np.in1d(y_train, 0), 1], 'o')
 plt.title("Train set")
+plt.savefig(os.path.join(image_dir, 'train_set.png'), bbox_inches='tight')
 plt.show()
 plt.plot(X_val[np.in1d(y_val, 1), 0], X_val[np.in1d(y_val, 1), 1], 'rX')
 plt.plot(X_val[np.in1d(y_val, 0), 0], X_val[np.in1d(y_val, 0), 1], 'o')
+plt.savefig(os.path.join(image_dir, 'validation_set.png'), bbox_inches='tight')
 plt.title("Validation set")
 plt.show()
 plt.title("Train set")
 plt.plot(X_test[np.in1d(y_test, 1), 0], X_test[np.in1d(y_test, 1), 1], 'rX')
 plt.plot(X_test[np.in1d(y_test, 0), 0], X_test[np.in1d(y_test, 0), 1], 'o')
+plt.savefig(os.path.join(image_dir, 'test_set.png'), bbox_inches='tight')
+
 plt.show()
 
 
 # # 1 - Multilayer Perceptron (MLP)
 
-# In[72]:
+# In[7]:
 
 
 C = 1 # number of classes
 D_in = 2 # dimension of input
 
 
-# In[73]:
+# In[8]:
 
 
-epochs = 100
-show_step = 10
-for H in [3, 6, 10, 30, 60, 100]:
+X_train_t = torch.tensor(X_train, dtype=torch.float)
+y_train_t = torch.tensor(y_train, dtype=torch.float)
+X_val_t = torch.tensor(X_val, dtype=torch.float)
+y_val_t = torch.tensor(y_val, dtype=torch.float)
+X_test_t = torch.tensor(X_test, dtype=torch.float)
+y_test_t = torch.tensor(y_test, dtype=torch.float)
+
+
+# In[ ]:
+
+
+# batch training
+epochs = 10000
+show_step = 1000
+
+for H in [3, 6, 10, 30, 60]:
     print("H = ", H)
     train_losses = []
     val_losses = []
@@ -105,6 +130,63 @@ for H in [3, 6, 10, 30, 60, 100]:
     model = torch.nn.Sequential(
         torch.nn.Linear(D_in, H),
         torch.nn.ReLU(),
+        torch.nn.Dropout(0.5),
+        torch.nn.Linear(H, C),
+        torch.nn.Sigmoid(),
+    )
+    
+    optimizer = optim.Adam(model.parameters())
+    
+    for epoch in range(epochs):
+        model.train()
+        optimizer.zero_grad()
+        output = model(X_train_t)
+        loss = F.binary_cross_entropy(output, y_train_t)
+        loss.backward()
+        optimizer.step()
+        train_losses.append(loss)
+        
+        model.eval()
+        with torch.no_grad():
+            output = model(X_val_t)
+            val_loss = F.binary_cross_entropy(output, y_val_t)
+        pred = np.where(output<0.5,0,1)
+        correct = y_val_t.eq(torch.tensor(pred, dtype=torch.float)).sum()
+        val_losses.append(val_loss)
+        if epoch % show_step == 0:
+            print('Validation set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+            val_loss, correct, len(y_val),
+            100. * correct / len(y_val)))
+        gc.collect()
+
+    
+    plt.plot(train_losses, color='blue')
+    plt.plot(val_losses, color='red')
+    plt.legend(["train", "validation"])
+    plt.yscale("log")
+    plt.title('Learning curve with {} neurons in hidden layer'.format(H))
+    plt.xlabel('number of training examples seen')
+    plt.ylabel('cross entropy loss')
+    plt.savefig(os.path.join(image_dir, 'batch_curve_h_{}.png'.format(H)), bbox_inches='tight')
+    plt.show()
+
+
+# In[ ]:
+
+
+# online training
+epochs = 200
+show_step = 20
+
+for H in [3, 6, 10, 30, 60]:
+    print("H = ", H)
+    train_losses = []
+    val_losses = []
+    
+    model = torch.nn.Sequential(
+        torch.nn.Linear(D_in, H),
+        torch.nn.ReLU(),
+        torch.nn.Dropout(0.5),
         torch.nn.Linear(H, C),
         torch.nn.Sigmoid(),
     )
@@ -120,8 +202,8 @@ for H in [3, 6, 10, 30, 60, 100]:
             loss = F.binary_cross_entropy(output, target)
             loss.backward()
             optimizer.step()
-        if epoch % show_step == 0:
-            print('Train Epoch: {} Loss: {:.6f}'.format(epoch, loss.item()))
+        # if epoch % show_step == 0:
+        #    print('Train Epoch: {} Loss: {:.6f}'.format(epoch, loss.item()))
         train_losses.append(loss.item())        
     
         model.eval()
@@ -136,69 +218,10 @@ for H in [3, 6, 10, 30, 60, 100]:
                 correct += (1 if pred == target else 0)
         val_loss /= len(y_val)
         val_losses.append(val_loss)
-    print('\nValidation set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        val_loss, correct, len(y_val),
-        100. * correct / len(y_val)))
-    
-    plt.plot(train_losses, color='blue')
-    plt.plot(val_losses, color='red')
-    plt.legend(["train", "validation"])
-    plt.yscale("log")
-    plt.title('Learning curve with {} neurons in hidden layer'.format(H))
-    plt.xlabel('number of training examples seen')
-    plt.ylabel('cross entropy loss')
-    plt.show()
-
-
-# In[53]:
-
-
-epochs = 200
-show_step = 10
-H = 60
-
-for H in [30, 40, 50, 60]:
-    print("H = ", H, "Learning rate:", learning_rate)
-    train_losses = []
-    val_losses = []
-    
-    model = torch.nn.Sequential(
-        torch.nn.Linear(D_in, H),
-        torch.nn.ReLU(),
-        torch.nn.Linear(H, C),
-        torch.nn.Sigmoid(),
-    )
-    
-    optimizer = optim.Adam(model.parameters())
-    
-    for epoch in range(epochs):
-        models.train()
-        for idx, (data, target) in enumerate(zip(torch.tensor(X_train, dtype=torch.float),
-                                         torch.tensor(y_train, dtype=torch.float))):
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.binary_cross_entropy(output, target)
-            loss.backward()
-            optimizer.step()
         if epoch % show_step == 0:
-            print('Train Epoch: {} Loss: {:.6f}'.format(epoch, loss.item()))
-        train_losses.append(loss.item())        
-    
-        model.eval()
-        val_loss = 0
-        correct = 0
-        with torch.no_grad():
-            for data, target in zip(torch.tensor(X_val, dtype=torch.float),
-                                             torch.tensor(y_val, dtype=torch.float)):
-                output = model(data)
-                val_loss += F.binary_cross_entropy(output, target, size_average=False).item()
-                pred = torch.round(output)
-                correct += (1 if pred == target else 0)
-        val_loss /= len(y_val)
-        val_losses.append(val_loss)
-    print('\nValidation set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        val_loss, correct, len(y_val),
-        100. * correct / len(y_val)))
+            print('Validation set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+                val_loss, correct, len(y_val),
+                100. * correct / len(y_val)))
     
     plt.plot(train_losses, color='blue')
     plt.plot(val_losses, color='red')
@@ -207,18 +230,20 @@ for H in [30, 40, 50, 60]:
     plt.title('Learning curve with {} neurons in hidden layer'.format(H))
     plt.xlabel('number of training examples seen')
     plt.ylabel('cross entropy loss')
+    plt.savefig(os.path.join(image_dir, 'online_curve_h_{}.png'.format(H)), bbox_inches='tight')
     plt.show()
 
 
-# In[86]:
+# In[ ]:
 
 
-epochs = 200
-H = 30
+epochs = 300
+H = 6
 
 model = torch.nn.Sequential(
         torch.nn.Linear(D_in, H),
         torch.nn.ReLU(),
+        torch.nn.Dropout(0.5),
         torch.nn.Linear(H, C),
         torch.nn.Sigmoid(),
 )
@@ -240,7 +265,7 @@ for epoch in range(epochs):
     print('.', end='')
 
 
-# In[87]:
+# In[ ]:
 
 
 model.eval()
@@ -254,12 +279,12 @@ with torch.no_grad():
         pred = torch.round(output)
         correct += (1 if pred == target else 0)
     test_loss /= len(y_test)
-print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+print('Test set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
     val_loss, correct, len(y_test),
     100. * correct / len(y_test)))
 
 
-# In[88]:
+# In[ ]:
 
 
 # Pass test data
@@ -298,45 +323,37 @@ plt.title("Decision Regions for MLP with {} neurons in hidden layer".format(H))
 plt.contourf(XX, YY, Z, cmap=plt.cm.coolwarm, alpha=0.5)
 plt.plot(X_test[np.in1d(y_test, 1), 0], X_test[np.in1d(y_test, 1), 1], 'rx')
 plt.plot(X_test[np.in1d(y_test, 0), 0], X_test[np.in1d(y_test, 0), 1], 'b.')
+plt.savefig(os.path.join(image_dir, 'mlp_regions_h_{}.png'.format(H)), bbox_inches='tight')
 plt.show()
 
 
 # ## 2 - Support-Vector Machine (SVM)
 
-# In[57]:
-
-
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
-from sklearn.model_selection import GridSearchCV
-from mlxtend.plotting import plot_decision_regions
-
-
-# In[58]:
+# In[ ]:
 
 
 clf = SVC(gamma='scale')
 
 
-# In[59]:
+# In[ ]:
 
 
 clf.fit(X_train, y_train.ravel())
 
 
-# In[60]:
+# In[ ]:
 
 
 y_pred = clf.predict(X_val)
 
 
-# In[61]:
+# In[ ]:
 
 
 accuracy_score(y_pred, y_val)
 
 
-# In[62]:
+# In[ ]:
 
 
 for kernel in ['linear', 'poly', 'rbf', 'sigmoid']:
@@ -357,7 +374,7 @@ for kernel in ['linear', 'poly', 'rbf', 'sigmoid']:
         plt.show()
 
 
-# In[63]:
+# In[ ]:
 
 
 clf = SVC(C=50, kernel='rbf', gamma='scale')
@@ -369,7 +386,7 @@ f1 = f1_score(y_test, y_pred)
 auc_score = roc_auc_score(y_test, y_pred)
 
 print('===')
-print('SVM with C =', c, 'and kernel =', kernel)
+print('SVM with C =', 50, 'and kernel =', 'rbf')
 print('Accuracy: {}, F1-score: {:.3f}, AUC: {:.3f}'.format(ac, f1, auc_score))
 
 plot_decision_regions(X_test, y_test.ravel(), clf=clf)
